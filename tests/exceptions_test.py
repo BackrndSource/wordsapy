551605112d378bb4299cc39efac53e95a021e518
backrndsource <backrndsource@gmail.com>
2020-12-29 05:25:45 +0100

    Update exceptions_test.py
    Deleted test that conflicts when running on TravisCI

--- tests/exceptions_test.py
+++ tests/exceptions_test.py
@@ -2,26 +2,15 @@ from unittest import TestCase
 from wordsapy import Dictionary, WordsapyException
 
 class WordsapyExceptionApiKeyTests(TestCase):
-    def setUp(self):
-        self.dictionary = Dictionary(api_key='')
-
     def test_exception_blank_api_key(self):
-        self.dictionary.api_key = ''
+        dictionary = Dictionary(api_key='')
         try:
-            self.dictionary.word('example')
+            dictionary.word('example')
         except WordsapyException as exception:
             self.assertTrue(isinstance(exception, WordsapyException))
             self.assertEqual(exception.args[0], 'API key not found.')
-    
-    def test_exception_not_valid_api_key(self):
-        self.dictionary.api_key = 'xxvvvvxxxxxxxxxxxxxxxxx'
-        try:
-            self.dictionary.word('example')
-        except WordsapyException as exception:
-            self.assertTrue(isinstance(exception, WordsapyException))
-            self.assertEqual(exception.args[0], 'You are not subscribed to this API.')
 
-class WordsapyExceptionTests(TestCase):
+class WordsapyExceptionBadQueriesTests(TestCase):
     def setUp(self):
         self.dictionary = Dictionary()
 
